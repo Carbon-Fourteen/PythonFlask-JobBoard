f67b29fc8c4815068c5b9db6f8b01d348cd7b771
Carbon-Fourteen <kylerbourgeous1@mail.weber.edu>
2019-03-15 19:57:23 -0600

    Preparing for Dynamic Content

--- jobs/app.py
+++ jobs/app.py
@@ -1,7 +1,48 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, g
+
+#Location of the database.
+#Hardcoded here so it can be reused.
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 
+
+def open_connection():
+	#Connect to the database if it isn't already connected.
+	connection = getattr(g, '_connection', None)
+	if connection == None:
+		connection = g._connection = sqlite3.connect(PATH)
+	#This makes row indexing easier.
+	connection.row_factory = sqlite3.Row
+	return connection
+	
+	
+def execute_sql(sql, values=(), commit=False, single=False):
+	#Used for easier execution of sql statements.
+	#Get the database connection.
+	connection = open_connection()
+	cursor = connection.execute(sql, values)
+	if commit == True:
+		results = connection.commit()
+	else:
+		#Ternary if based on number of results obtained.
+		results = cursor.fetchone() if single else cursor.fetchall()
+		
+	cursor.close()
+	return results
+	
+	
+#Calls close_connection when the app context is destroyed.
+@app.teardown_appcontext
+def close_connection(exception):
+	connection = getattr(g, '_connection', None)
+	if connection is not None:
+		connection.close()
+
+		
+#Decorators
+#Decorators can be used to inject additional functionality to one or more functions.
 @app.route('/')
 @app.route('/jobs')
 def jobs():
